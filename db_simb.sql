247eb468011eefd05754e34f839c4964d7700984
gerinsp <denapratama7@gmail.com>
2023-11-09 22:31:25 +0700

    update db

--- db_simb.sql
+++ db_simb.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Nov 2023 pada 16.45
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 7.4.29
+-- Generation Time: Nov 09, 2023 at 04:30 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 5.6.40
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,7 +25,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_mekanik`
+-- Table structure for table `tbl_booking`
+--
+
+CREATE TABLE `tbl_booking` (
+  `id_booking` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `tipe_kendaraan` varchar(255) NOT NULL,
+  `plat_nomor` varchar(255) NOT NULL,
+  `id_tipe_service` int(11) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `is_delivery` tinyint(1) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `down_payment_image` longtext NOT NULL,
+  `id_status_booking` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_invoice`
+--
+
+CREATE TABLE `tbl_invoice` (
+  `no_invoice` bigint(20) NOT NULL,
+  `id_service` int(11) NOT NULL,
+  `nama_customer` varchar(255) NOT NULL,
+  `tanggal` date NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `total_harga` bigint(20) NOT NULL,
+  `down_payment` bigint(20) NOT NULL,
+  `rest_bill` bigint(20) NOT NULL,
+  `status` enum('paid','unpaid') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kuitansi`
+--
+
+CREATE TABLE `tbl_kuitansi` (
+  `no_kuitansi` bigint(20) NOT NULL,
+  `no_invoice` bigint(20) NOT NULL,
+  `nama_customer` varchar(255) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama_service` varchar(255) NOT NULL,
+  `total_harga` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mekanik`
 --
 
 CREATE TABLE `tbl_mekanik` (
@@ -36,7 +89,7 @@ CREATE TABLE `tbl_mekanik` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tbl_mekanik`
+-- Dumping data for table `tbl_mekanik`
 --
 
 INSERT INTO `tbl_mekanik` (`id_mekanik`, `nama_mekanik`, `tanggal_lahir`, `alamat`, `telepon`) VALUES
@@ -45,7 +98,20 @@ INSERT INTO `tbl_mekanik` (`id_mekanik`, `nama_mekanik`, `tanggal_lahir`, `alama
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_role`
+-- Table structure for table `tbl_pengeluaran`
+--
+
+CREATE TABLE `tbl_pengeluaran` (
+  `id_pengeluaran` int(11) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `total_harga` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_role`
 --
 
 CREATE TABLE `tbl_role` (
@@ -54,7 +120,7 @@ CREATE TABLE `tbl_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tbl_role`
+-- Dumping data for table `tbl_role`
 --
 
 INSERT INTO `tbl_role` (`id`, `role`) VALUES
@@ -64,7 +130,54 @@ INSERT INTO `tbl_role` (`id`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_user`
+-- Table structure for table `tbl_service`
+--
+
+CREATE TABLE `tbl_service` (
+  `id_service` int(11) NOT NULL,
+  `id_booking` int(11) NOT NULL,
+  `nama_customer` varchar(255) NOT NULL,
+  `tgl_mulai` datetime NOT NULL,
+  `tgl_selesai` datetime NOT NULL,
+  `tipe_kendaraan` varchar(255) NOT NULL,
+  `plat_nomor` varchar(255) NOT NULL,
+  `nama_service` varchar(255) NOT NULL,
+  `id_mekanik` int(11) NOT NULL,
+  `total_harga` bigint(20) NOT NULL,
+  `deskripsi` varchar(255) NOT NULL,
+  `status` enum('Menunggu Kedatangan','Proses Perbaikan','Selesai') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_status_booking`
+--
+
+CREATE TABLE `tbl_status_booking` (
+  `id_status_booking` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tipe_service`
+--
+
+CREATE TABLE `tbl_tipe_service` (
+  `id_tipe_service` int(11) NOT NULL,
+  `nama_service` varchar(255) NOT NULL,
+  `down_payment` bigint(20) NOT NULL,
+  `estimasi_hari` varchar(255) NOT NULL,
+  `estimasi_harga` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
 --
 
 CREATE TABLE `tbl_user` (
@@ -79,7 +192,7 @@ CREATE TABLE `tbl_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tbl_user`
+-- Dumping data for table `tbl_user`
 --
 
 INSERT INTO `tbl_user` (`id_user`, `nama`, `username`, `image`, `password`, `role_id`, `is_active`, `tanggal_daftar`) VALUES
@@ -90,44 +203,116 @@ INSERT INTO `tbl_user` (`id_user`, `nama`, `username`, `image`, `password`, `rol
 --
 
 --
--- Indeks untuk tabel `tbl_mekanik`
+-- Indexes for table `tbl_booking`
+--
+ALTER TABLE `tbl_booking`
+  ADD PRIMARY KEY (`id_booking`);
+
+--
+-- Indexes for table `tbl_invoice`
+--
+ALTER TABLE `tbl_invoice`
+  ADD PRIMARY KEY (`no_invoice`);
+
+--
+-- Indexes for table `tbl_kuitansi`
+--
+ALTER TABLE `tbl_kuitansi`
+  ADD PRIMARY KEY (`no_kuitansi`);
+
+--
+-- Indexes for table `tbl_mekanik`
 --
 ALTER TABLE `tbl_mekanik`
   ADD PRIMARY KEY (`id_mekanik`);
 
 --
--- Indeks untuk tabel `tbl_role`
+-- Indexes for table `tbl_pengeluaran`
+--
+ALTER TABLE `tbl_pengeluaran`
+  ADD PRIMARY KEY (`id_pengeluaran`);
+
+--
+-- Indexes for table `tbl_role`
 --
 ALTER TABLE `tbl_role`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `tbl_user`
+-- Indexes for table `tbl_service`
+--
+ALTER TABLE `tbl_service`
+  ADD PRIMARY KEY (`id_service`);
+
+--
+-- Indexes for table `tbl_status_booking`
+--
+ALTER TABLE `tbl_status_booking`
+  ADD PRIMARY KEY (`id_status_booking`);
+
+--
+-- Indexes for table `tbl_tipe_service`
+--
+ALTER TABLE `tbl_tipe_service`
+  ADD PRIMARY KEY (`id_tipe_service`);
+
+--
+-- Indexes for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_mekanik`
+-- AUTO_INCREMENT for table `tbl_booking`
+--
+ALTER TABLE `tbl_booking`
+  MODIFY `id_booking` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_mekanik`
 --
 ALTER TABLE `tbl_mekanik`
   MODIFY `id_mekanik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_role`
+-- AUTO_INCREMENT for table `tbl_pengeluaran`
 --
-ALTER TABLE `tbl_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+ALTER TABLE `tbl_pengeluaran`
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_user`
+-- AUTO_INCREMENT for table `tbl_role`
+--
+ALTER TABLE `tbl_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_service`
+--
+ALTER TABLE `tbl_service`
+  MODIFY `id_service` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_status_booking`
+--
+ALTER TABLE `tbl_status_booking`
+  MODIFY `id_status_booking` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_tipe_service`
+--
+ALTER TABLE `tbl_tipe_service`
+  MODIFY `id_tipe_service` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
