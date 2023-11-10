bab0ebf8ad586c49227f1bd7ec290f917160491b
sejutaimpian <derissulistina@gmail.com>
2023-11-10 16:18:13 +0700

    menambahkan page home, edit default route, dan menambahkan beberapa aset

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'auth';
-
+$route['default_controller'] = 'home';
 
 $route['login']      = 'auth/index';
 $route['blocked']    = 'auth/blocked';
